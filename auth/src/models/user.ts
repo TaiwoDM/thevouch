b832f789183da3bfff856b7a54b84b2e56661c9e
Daniel <danielmayowataiwo@gmail.com>
2022-09-21 09:19:51 +0100

    simple user model design

---
+++ auth/src/models/user.ts
@@ -0,0 +1,37 @@
+import mongoose from 'mongoose';
+
+// interface describing props needed to create user
+interface UserAttributes {
+  email: string;
+  password: string;
+}
+
+// interface describing the methods that the user model has to have
+interface UserModel extends mongoose.Model<UserDoc> {
+  build(userAttributes: UserAttributes): UserDoc;
+}
+
+// interface describing the properties of a user document
+interface UserDoc extends mongoose.Document {
+  email: string;
+  password: string;
+}
+
+const userSchema = new mongoose.Schema({
+  email: {
+    type: String,
+    required: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+});
+
+userSchema.statics.build = function (userAttributes: UserAttributes) {
+  return new User(userAttributes);
+};
+
+const User = mongoose.model<UserDoc, UserModel>('User', userSchema);
+
+export { User };
