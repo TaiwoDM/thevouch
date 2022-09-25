ac7821c5bb66fdeea6923de99627aa303f84c4f0
Daniel <danielmayowataiwo@gmail.com>
2022-09-25 23:33:54 +0100

    signout and current user routes handler implementation

--- auth/src/routes/current-user.ts
+++ auth/src/routes/current-user.ts
@@ -1,9 +1,14 @@
-import express from 'express';
+import express, { Request, Response, NextFunction } from 'express';
+import { currentUser } from '../middlewares/current-user';
 
 const router = express.Router();
 
-router.get('/api/users/currentuser', (req, res, next) => {
-  res.send('Hi there!');
-});
+router.get(
+  '/api/users/currentuser',
+  currentUser,
+  (req: Request, res: Response, next: NextFunction) => {
+    res.send({ currentUser: req.currentUser || null });
+  }
+);
 
 export { router as currentUserRouter };
