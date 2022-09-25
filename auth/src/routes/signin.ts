98537ef301d8f9a826cf03addf748d8fdb29918f
Daniel <danielmayowataiwo@gmail.com>
2022-09-25 21:46:33 +0100

    sign in route handler implemented

--- auth/src/routes/signin.ts
+++ auth/src/routes/signin.ts
@@ -1,9 +1,57 @@
-import express from 'express';
+import express, { Request, Response, NextFunction } from 'express';
+import jwt from 'jsonwebtoken';
+import { body, validationResult } from 'express-validator';
+
+import { BadRequestError } from '../errors/bad-request-error';
+import { RequestValidationError } from '../errors/request-validation-error';
+import { User } from '../models/user';
+import { Password } from './../services/password';
 
 const router = express.Router();
 
-router.post('/api/users/signin', (req, res, next) => {
-  res.send('Hi there!');
-});
+router.post(
+  '/api/users/signin',
+  [
+    body('email').isEmail().withMessage('Email must be valid'),
+    body('password')
+      .trim()
+      .notEmpty()
+      .withMessage('You must supply a password'),
+  ],
+  async (req: Request, res: Response, next: NextFunction) => {
+    const errors = validationResult(req);
+
+    if (!errors.isEmpty()) {
+      throw new RequestValidationError(errors.array());
+    }
+
+    const { email, password } = req.body;
+
+    const existingUser = await User.findOne({ email });
+
+    if (!existingUser) {
+      throw new BadRequestError('Invalid credentials!');
+    }
+
+    const correctPassword = await Password.compare(
+      existingUser.password,
+      password
+    );
+
+    if (!correctPassword) {
+      throw new BadRequestError('Invalid credentials!');
+    }
+
+    // generate token
+    const userJwt = jwt.sign(
+      { id: existingUser.id, email: existingUser.email },
+      process.env.JWT_KEY!
+    );
+
+    req.session = { jwt: userJwt };
+
+    res.status(200).send(existingUser);
+  }
+);
 
 export { router as signinRouter };
