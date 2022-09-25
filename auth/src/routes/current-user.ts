1b321f629ef2ac3ce40c4ad537d9549527104269
Daniel <danielmayowataiwo@gmail.com>
2022-09-25 23:38:49 +0100

    requireauth middleware to protect route from not authenticated entities

--- auth/src/routes/current-user.ts
+++ auth/src/routes/current-user.ts
@@ -1,11 +1,13 @@
 import express, { Request, Response, NextFunction } from 'express';
 import { currentUser } from '../middlewares/current-user';
+import { requireAuth } from '../middlewares/require-auth';
 
 const router = express.Router();
 
 router.get(
   '/api/users/currentuser',
   currentUser,
+  requireAuth,
   (req: Request, res: Response, next: NextFunction) => {
     res.send({ currentUser: req.currentUser || null });
   }
