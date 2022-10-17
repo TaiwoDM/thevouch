4cd85cf55afe7b45ba814456aa1ed07007202abb
Daniel <danielmayowataiwo@gmail.com>
2022-10-17 20:39:39 +0100

    made changes to mongo connection string using env variable to connect

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -7,11 +7,12 @@ const start = async () => {
   if (!process.env.JWT_KEY) {
     throw new Error('JWT_KEY must be defined');
   }
+  if (!process.env.MONGO_URI) {
+    throw new Error('MONGO_URI must be defined');
+  }
 
   try {
-    const connect = await mongoose.connect(
-      'mongodb://auth-mongo-srv:27017/auth'
-    );
+    const connect = await mongoose.connect(process.env.MONGO_URI);
     console.log('Connected to mongodb');
   } catch (err) {
     console.log('something went wrong while connecting to db');
