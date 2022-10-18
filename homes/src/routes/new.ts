16e21464098a73d2df21f2082e90253dbed234c7
Daniel <danielmayowataiwo@gmail.com>
2022-10-18 16:08:50 +0100

    added create new home route handler

---
+++ homes/src/routes/new.ts
@@ -0,0 +1,34 @@
+import express, { Request, Response } from 'express';
+import { requireAuth, validationRequest } from '@gethomes/common';
+import { body } from 'express-validator';
+
+import { Home } from '../models/homes';
+
+const router = express.Router();
+
+router.post(
+  '/api/tickets',
+  requireAuth,
+  [
+    body('title').not().isEmpty().withMessage('Title is required'),
+    body('price')
+      .isFloat({ gt: 0 })
+      .withMessage('Price must be greater than 0'),
+  ],
+  validationRequest,
+  async (req: Request, res: Response) => {
+    const { description, price, picture } = req.body;
+
+    const home = Home.build({
+      description,
+      picture,
+      price,
+      userId: req.currentUser!.id,
+    });
+    await home.save();
+
+    res.status(201).send(home);
+  }
+);
+
+export { router as createHomeRouter };
