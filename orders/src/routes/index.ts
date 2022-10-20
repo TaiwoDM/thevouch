c690eb129d81cc986e087cb4de1adfa5bea9ebc1
Daniel <danielmayowataiwo@gmail.com>
2022-10-20 18:00:39 +0100

    implemented route to fetch all orders by a particular user

--- orders/src/routes/index.ts
+++ orders/src/routes/index.ts
@@ -1,9 +1,16 @@
 import express, { Request, Response } from 'express';
+import { requireAuth } from '@gethomes/common';
+
+import { Order } from '../model/order';
 
 const router = express.Router();
 
-router.get('api/orders', async (req: Request, res: Response) => {
-  res.send({});
+router.get('api/orders', requireAuth, async (req: Request, res: Response) => {
+  const orders = await Order.find({
+    userId: req.currentUser!.id,
+  }).populate('home');
+
+  res.send(orders);
 });
 
 export { router as indexOrderRouter };
