322ee104645c744b7dd2fc9b6b4564031b3eb679
Daniel <danielmayowataiwo@gmail.com>
2022-10-20 17:46:13 +0100

    unfinished new order creation route implementation

--- orders/src/routes/new.ts
+++ orders/src/routes/new.ts
@@ -1,23 +1,62 @@
 import mongoose from 'mongoose';
 import express, { Request, Response } from 'express';
-import { requireAuth, validationRequest } from '@gethomes/common';
+import {
+  requireAuth,
+  validationRequest,
+  NotFoundError,
+  BadRequestError,
+} from '@gethomes/common';
 import { body } from 'express-validator';
 
+import { Order, OrderStatus } from '../model/order';
+import { Home } from '../model/home';
+
 const router = express.Router();
+
+const EXPIRATION_WINDOW_SECONDS = 10 * 60;
 
 router.post(
   '/api/orders',
   requireAuth,
   [
-    body('ticketId')
+    body('homeId')
       .not()
       .isEmpty()
       .custom((input: string) => mongoose.Types.ObjectId.isValid(input))
-      .withMessage('TicketId must be provided'),
+      .withMessage('homeId must be provided'),
   ],
   validationRequest,
   async (req: Request, res: Response) => {
-    res.send({});
+    const { homeId } = req.body;
+
+    //// Find the home the user is trying to order in the database
+    const home = await Home.findById(homeId);
+    if (!home) {
+      throw new NotFoundError();
+    }
+
+    //// Make sure that this home is not already reserved
+    const isReserved = await home.isReserved();
+    if (isReserved) {
+      throw new BadRequestError('This home is already reserved');
+    }
+
+    // Calculate an expiration date for this order
+    const expiration = new Date();
+    expiration.setSeconds(expiration.getSeconds() + EXPIRATION_WINDOW_SECONDS);
+
+    // Build the order and save it to the database
+    const order = Order.build({
+      userId: req.currentUser!.id,
+      status: OrderStatus.Created,
+      expiresAt: expiration,
+      home,
+    });
+    await order.save();
+
+    // Publish an event saying that an order was created
+
+    res.status(201).send({});
   }
 );
 
