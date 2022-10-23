351e758a2f1d694bfe1dafa634874660542e8482
Daniel <danielmayowataiwo@gmail.com>
2022-10-23 04:54:30 +0100

    update expiration timer to 10mins

--- orders/src/routes/new.ts
+++ orders/src/routes/new.ts
@@ -15,7 +15,7 @@ import { natsWrapper } from '../nats_wrapper';
 
 const router = express.Router();
 
-const EXPIRATION_WINDOW_SECONDS = 1 * 60;
+const EXPIRATION_WINDOW_SECONDS = 10 * 60;
 
 router.post(
   '/api/orders',
